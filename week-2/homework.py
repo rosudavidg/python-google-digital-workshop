3497983bfdd3b782d5b898297c335aeb49be3f67
rosudavidg <rosudavidg@gmail.com>
2021-05-20 19:02:21 +0300

    Add comments; Fix task

--- week-2/homework.py
+++ week-2/homework.py
@@ -1,11 +1,17 @@
+# Init list
 l = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 
+# Print sorted list
 print(sorted(l))
 
+# Prin sorted list in descending order
 print(sorted(l, reverse=True))
 
+# Print only even index
 print(l[::2])
 
+# Print only odd index
 print(l[1::2])
 
-print(l[::3])
+# Print 3-multipliers
+print(list(filter(lambda e: e % 3 == 0, l)))
